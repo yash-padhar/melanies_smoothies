7a74ca0b4198fc970aba19b399e177ea5309a80b
yash-padhar <165518735+yash-padhar@users.noreply.github.com>
2024-05-30 19:28:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,9 @@ st.write(
     """Choose the fruits you want in your custom Smoothie!
     """)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
+pd_df=my_dataframe.to_pandas()
 
 ingredients_list=st.multiselect(
     'Choose upto 5 ingridents:'
@@ -32,6 +32,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0] 
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen+' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
